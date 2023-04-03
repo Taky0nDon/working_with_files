29c55d4cdf17ec0c5c0ff9a66700724a110f1ece
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-04-03 17:11:23 -0400

    added try/except for creation of new directory in case of invalid input causing a FileNotFoundError

--- working_with_files/main.py
+++ working_with_files/main.py
@@ -1,10 +1,11 @@
-from os import system
-home = "C:\\Users\\Mourn\\100_days_of_code"
+import os
+home = "C:/Users/Mourn/100_days_of_code"
+os.chdir = home
 print(f"The current directory is {home}")
 change = input("Do you wish to change it? y/n: ")
 make_sure = 0
 if change == 'y':
-    #this doesnt work
+    # this doesn't work
     # scan = input("Do you want to scan for a new directory? y/n: ")
     # if scan == 'y':
     #     path1 = "C:/"
@@ -19,15 +20,24 @@ if change == 'y':
         print(f"You entered {new_dir}")
         make_sure = input("Is this correct? y/n")
 else:
-    new_dir =input("Enter the name of the new directory to create: ")
-path = f"{home}\\{new_dir}"
-os.mkdir(path)
+    try:
+        new_dir = input("Enter the name of the new directory to create: ")
+        path = f"{home}/{new_dir}"
+        os.mkdir(path)
+    except FileNotFoundError:
+        not_valid = True
+        while not_valid:
+            print("You did not enter a valid directory name.")
+            new_dir = input("Enter the name of the new directory to create: ")
+            path = f"{home}/{new_dir}"
+            os.mkdir(path)
+            not_valid = False
 print(f"Created new directory {path}")
-cd = os.chdir(path)
+os.chdir = path
 print(f"The current directory is {path}")
 f_name = input("Enter the name of the file you wish to create (include extension): ")
 file = open(f_name, "w")
 first_line = input("Enter the first line of the new file: ")
 file.write(first_line)
-print(f"{f_name} has been created in {cd} with the first line of {first_line}")
+print(f"{f_name} has been created in {path} with the first line of {first_line}")
 file.close()
