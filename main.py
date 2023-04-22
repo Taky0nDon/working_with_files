8e7d98ba9d23104bcabf506224841b536f17d927
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-04-22 07:51:46 -0400

    switched to pathlib
    i changed it to work with the pathlib library. I also added an exception in case the user attempts to create a directory that already exists. the large comment block near the top was me trying to let the user set a new home directory.

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-import os
-home = "C:\\Users\\Mourn\\100_days_of_code"
+import pathlib
+home = pathlib.Path("C:/Users/Mourn/100_days_of_code")
+# os.chdir(home)
+# pwd = os.getcwd()
 print(f"The current directory is {home}")
-new_dir =input("Enter the name of the new directory to create: ")
-path = f"{home}\\{new_dir}"
-os.mkdir(path)
-print(f"Created new directory {path}")
-cd = os.chdir(path)
-print(f"The current directory is {path}")
+change = input("Do you wish to change it? y/n: ")
+make_sure = 0
+if change == 'y':
+    # this doesn't work
+    # scan = input("Do you want to scan for a new directory? y/n: ")
+    # if scan == 'y':
+    #     path1 = "C:/"
+    #     system('dir {path1}')
+    #     next = input("Choose another fold to scan? y/n: ")
+    #     if next == 'y':
+    #         next_dir = input("Enter the directory to scan")
+    #         system('dir {path+"next_dir"}')
+
+    while make_sure != 'y':
+        new_dir = input("Enter the correct path: ")
+        print(f"You entered {new_dir}")
+        make_sure = input("Is this correct? y/n")
+else:
+    try:
+        new_dir = input("Enter the name of the new directory to create: ")
+        new_path = pathlib.Path(f"{home}/{new_dir}")
+        new_path.mkdir()
+    except FileExistsError:
+        not_valid = True
+        while not_valid:
+            print("You did not enter a valid directory name.")
+            new_dir = input("Enter the name of the new directory to create: ")
+            # new_path = f"{home}/{new_dir}"
+            home.mkdir(new_dir)
+            not_valid = False
+print(f"Created new directory {new_path}")
+# os.chdir(path)
+# pwd = os.getcwd()
+print(f"The current directory is {new_path}")
 f_name = input("Enter the name of the file you wish to create (include extension): ")
-file = open(f_name, "w")
-first_line = input("Enter the first line of the new file: ")
-file.write(first_line)
-print(f"{f_name} has been created in {path} with the first line of {first_line}")
-file.close()
+with open(f"{new_path}/{f_name}", "w") as file:
+    first_line = input("Enter the first line of the new file: ")
+    file.write(first_line)
+print(f"{f_name} has been created in {new_path} with the first line of {first_line}")
