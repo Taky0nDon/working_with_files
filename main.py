0878e103a04581b20e32a6a7654033fd3b70b2bf
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-07-21 17:57:57 -0400

    new file is now saved in my obsidian folder as well

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import pathlib
+
+OBSIDIAN_DIR = r"C:\Users\Mourn\Documents\100 Days of Code\Directory"
+start = "C:/Users/Mourn/100_days_of_code"
 home = pathlib.Path("C:/Users/Mourn/100_days_of_code")
-# os.chdir(home)
-# pwd = os.getcwd()
-print(f"The current directory is {home}")
-change = input("Do you wish to change it? y/n: ")
-make_sure = 0
-if change == 'y':
-    # this doesn't work
-    # scan = input("Do you want to scan for a new directory? y/n: ")
-    # if scan == 'y':
-    #     path1 = "C:/"
-    #     system('dir {path1}')
-    #     next = input("Choose another fold to scan? y/n: ")
-    #     if next == 'y':
-    #         next_dir = input("Enter the directory to scan")
-    #         system('dir {path+"next_dir"}')
+
+print(f"New directory will be created in {home}.")
+change = input("Create file in a different directory? y/n: ")
+make_sure = "y"
+if change.lower() == 'y':
+    new_home = input("Please enter the correct working directory: ")
+    home = pathlib.Path(new_home)
 
     while make_sure != 'y':
         new_dir = input("Enter the correct path: ")
         print(f"You entered {new_dir}")
         make_sure = input("Is this correct? y/n")
-else:
+if make_sure == "y":
     try:
         new_dir = input("Enter the name of the new directory to create: ")
         new_path = pathlib.Path(f"{home}/{new_dir}")
@@ -34,11 +29,16 @@ else:
             home.mkdir(new_dir)
             not_valid = False
 print(f"Created new directory {new_path}")
-# os.chdir(path)
-# pwd = os.getcwd()
-print(f"The current directory is {new_path}")
+
+
+print(f"New file will be added to {new_path} and {OBSIDIAN_DIR}.")
+
+directories = [OBSIDIAN_DIR, new_path]
 f_name = input("Enter the name of the file you wish to create (include extension): ")
-with open(f"{new_path}/{f_name}", "w") as file:
-    first_line = input("Enter the first line of the new file: ")
-    file.write(first_line)
-print(f"{f_name} has been created in {new_path} with the first line of {first_line}")
+first_line = input("Enter the first line of the new file: ")
+
+for directory in directories:
+    path = directory
+    with open(f"{path}/{f_name}", "w") as file:
+        file.write(first_line)
+print(f"{f_name} has been created in {new_path} and {OBSIDIAN_DIR} with the first line of {first_line}")
