2754012e61c2b240c3ecdebbb178de4f5705c489
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-09-22 20:28:27 -0400

    script now runs as admin if shell not already admin

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 import pathlib
+import time
+import os
+import pyuac
 
-OBSIDIAN_DIR = r"C:\Users\Mourn\Documents\100 Days of Code\Directory"
-start = "C:/Users/Mourn/100_days_of_code"
+OBSIDIAN_DIR = pathlib.Path(r"C:\Users\Mourn\Documents\obsidian vault\100 Days Of Code")
 home = pathlib.Path("C:/Users/Mourn/100_days_of_code")
+invalid_input = True
 
-print(f"New directory will be created in {home}.")
-change = input("Create file in a different directory? y/n: ")
-make_sure = "y"
-if change.lower() == 'y':
-    new_home = input("Please enter the correct working directory: ")
-    home = pathlib.Path(new_home)
+if __name__ == "__main__":
+    if not pyuac.isUserAdmin():
+        print("Relaunching as admin.")
+        pyuac.runAsAdmin()
 
-    while make_sure != 'y':
-        new_dir = input("Enter the correct path: ")
-        print(f"You entered {new_dir}")
-        make_sure = input("Is this correct? y/n")
-if make_sure == "y":
-    try:
+    print(f"New directory will be created in {home}.")
+
+    while invalid_input:
         new_dir = input("Enter the name of the new directory to create: ")
-        new_path = pathlib.Path(f"{home}/{new_dir}")
-        new_path.mkdir()
-    except FileExistsError:
-        not_valid = True
-        while not_valid:
-            print("You did not enter a valid directory name.")
-            new_dir = input("Enter the name of the new directory to create: ")
-            # new_path = f"{home}/{new_dir}"
-            home.mkdir(new_dir)
-            not_valid = False
-print(f"Created new directory {new_path}")
-
-
-print(f"New file will be added to {new_path} and {OBSIDIAN_DIR}.")
-
-directories = [OBSIDIAN_DIR, new_path]
-f_name = input("Enter the name of the file you wish to create (include extension): ")
-first_line = input("Enter the first line of the new file: ")
-
-for directory in directories:
-    path = directory
-    with open(f"{path}/{f_name}", "w") as file:
+        new_path = pathlib.Path(home, new_dir)
+        try:
+            os.mkdir(new_path)
+        except FileExistsError as e:
+                print(e)
+                print("You did not enter a valid directory name.")
+        else:
+            invalid_input = False
+    print(f"Created new directory {new_path}")
+    print(f"\n{'*'*80}\n")
+    f_name = input("Enter the name of the file you wish to create (include extension): ")
+    print(f"{f_name} will be added to {new_path} and a symlink pointing to it in {OBSIDIAN_DIR}.")
+    first_line = input("Enter the first line of the new file: ")
+    original_file = pathlib.Path(new_path, f_name)
+    with open(original_file, "w") as file:
         file.write(first_line)
-print(f"{f_name} has been created in {new_path} and {OBSIDIAN_DIR} with the first line of {first_line}")
+    symlink = pathlib.Path(OBSIDIAN_DIR, f_name)
+    os.symlink(src=original_file, dst=symlink)
+    print(f"{f_name} has been created in {new_path} and {OBSIDIAN_DIR} with the first line of {first_line}")
+    print("window will close in 3 seconds.")
+    print("***END***")
+    time.sleep(3)
